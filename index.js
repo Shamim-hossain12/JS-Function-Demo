3b755b6aac594f2d714b60246ee5de45265e48c7
Shamim-hossain12 <mdshamimhossain62273295@gmail.com>
2023-02-18 16:41:07 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let a = 50 + 25;
+
+function plus(p1, p2) {
+    document.querySelector('h2').innerText = a;
+}
+let b = 50 - 25;
+
+function mainus(p1, p2) {
+    document.querySelector('h2').innerText = b;
+}
+let s = 50 * 25;
+
+function into(p1, p2) {
+    document.querySelector('h2').innerHTML = s;
+}
+let d = 50 / 25;
+
+function divided(p1, p2) {
+    document.querySelector('h2').innerHTML = d;
+}
